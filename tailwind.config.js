597be9f73fc95b7ea9f1dafcd6009693b297a7c9
Luca Di Marco <lucanahtsu@gmail.com>
2023-09-26 21:48:01 -0300

    Add Tailwind to the project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html, js}'],
+  theme: {
+    fontFamily: {
+      'montserrat': ['Montserrat', 'sans'],
+      'fraunces': ['Fraunces', 'serif']
+    },
+    extend: {
+      colors: {
+        'darkcyan': '#3c8067',
+        'cream': '#f2ebe3',
+        'verydarkblue': '#1c232b',
+        'darkgrayish': '	#6c7289',
+      },
+  
+    },
+  },
+  plugins: [],
+}
+
